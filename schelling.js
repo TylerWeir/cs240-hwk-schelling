6321d942ab84137d9d60bb150f770a93022efd3d
TylerWeir <tylerw@live.com>
2021-10-21 20:50:01 -0700

    Dynamically generate the board.

--- schelling.js
+++ schelling.js
@@ -1,4 +1,36 @@
 /**
  * Schelling's Model simulator
- * @author
+ * @author Tyler Weir
  */
+
+const cell = "<div id='cell'></div>"
+
+/**
+ * Creates the table based on the parameters set by the user. 
+ */
+function createTable() {
+	let size = document.getElementById("dimension").value;
+
+	// Generate the HTML for the table
+	var table = "<table>";
+	for (let i = 0; i < size; i++) {
+		table += "<tr>"
+		for (let j = 0; j < size; j++) {
+			table += "<td>"+cell+"</td>";
+		}
+		table += "</tr>";
+	}
+	
+	let board = document.getElementById("board");
+	board.innerHTML = table;
+}
+
+
+
+// EVENT LISTENERS
+
+document.getElementById("dimension").addEventListener("change", createTable);
+
+
+
+
